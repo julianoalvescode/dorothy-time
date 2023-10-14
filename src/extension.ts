7a6653786371fa1d9bd848f5ad74cebde6a93e69
Juliano Alves <contato@julianoalves.me>
2023-10-14 16:04:33 -0300

    feat: 🎸 first version

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,63 @@
 import * as vscode from "vscode";
-import * as fs from "fs";
 
-let focusTime: Date | null = null;
 let statusBar: vscode.StatusBarItem;
+let projectStartTime: Date | null = null;
+let intervalId: NodeJS.Timeout | null = null;
 
 export function activate(context: vscode.ExtensionContext) {
   statusBar = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Right,
     100
   );
-  statusBar.text = "Time Spent: 0s";
   statusBar.show();
 
+  if (vscode.workspace.workspaceFolders) {
+    projectStartTime = new Date();
+    startUpdatingStatusBar();
+  }
+
   context.subscriptions.push(
-    vscode.window.onDidChangeActiveTextEditor((editor) => {
-      if (editor) {
-        // Editor focused
-        focusTime = new Date();
-      } else if (focusTime) {
-        const focusDuration =
-          (new Date().getTime() - focusTime.getTime()) / 1000;
-        statusBar.text = `Time Spent: ${focusDuration}s`;
-        console.log(focusDuration);
-        focusTime = null;
+    vscode.workspace.onDidChangeWorkspaceFolders((event) => {
+      if (event.added.length > 0) {
+        projectStartTime = new Date();
+        startUpdatingStatusBar();
+      } else if (
+        event.removed.length > 0 &&
+        !vscode.workspace.workspaceFolders
+      ) {
+        stopUpdatingStatusBar();
+        projectStartTime = null;
+        statusBar.text = "Time Spent: 0s";
       }
     })
   );
 }
 
+function startUpdatingStatusBar() {
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+  intervalId = setInterval(updateStatusBar, 1000);
+}
+
+function stopUpdatingStatusBar() {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+}
+
+function updateStatusBar() {
+  if (projectStartTime) {
+    const elapsedTime = new Date().getTime() - projectStartTime.getTime();
+    const hours = Math.floor(elapsedTime / 3600000);
+    const minutes = Math.floor((elapsedTime % 3600000) / 60000);
+    const seconds = Math.floor((elapsedTime % 60000) / 1000);
+    statusBar.text = `Time Spent: ${hours}h ${minutes}m ${seconds}s`;
+  }
+}
+
 export function deactivate() {
-  statusBar.hide();
+  stopUpdatingStatusBar();
   statusBar.dispose();
 }
